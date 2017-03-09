39325e42bfc9b1d6ba9ddd8bd4c1367cf7e873fa
Jean-Marc <jean-marc@lg21.be>
2017-03-09 11:48:05 +0100

    converts structure to html5

--- index.php
+++ index.php
@@ -30,10 +30,10 @@ if ( $directory = opendir($path."/".$current_episode."/") ) {
 	sort($files);
 	$filesnbr = count($files);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="<?php echo $Lang ?>">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8">
 <title><?php echo $episode[$ep].' - '.$Title; ?></title>
 <link rel="stylesheet" type="text/css" href="design/style.php" />
 <link rel="image_src" href="<?php echo $FacebookImageUrl; ?>" type="image/x-icon" />
