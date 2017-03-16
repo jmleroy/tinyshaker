ff70e9538d69fe63e650c3bae2a0ae7a8c05ebba
Jean-Marc <jean-marc@lg21.be>
2017-03-16 17:26:12 +0100

    removes .htaccess from versioned files and fixes htaccess example for url rewriting

--- config_default.inc.php
+++ config_default.inc.php
@@ -21,8 +21,15 @@ $Url = "http://julien.falgas.fr/tinyshaker/";
 $FacebookImageUrl = "http://julien.falgas.fr/tinyshaker/thumbnail.png";
 $Description = "Script PHP/Javascript de publication en ligne de récit sous forme de diaporama.";
 
-//set to '0' if your host does not manage url rewriting properly and if the links between chapters do not work. In that case, replace the contents of the file .htaccess by the contents of "htaccess_pour_free-fr" if your site is hosted by Free.fr, or try to delete that file.
-//passer à '0' si votre hébergeur ne gère pas convenablement la réécriture d'url et que les liens entre les chapitres ne fonctionnent pas. Dans ce cas, remplacez le fichier .htaccess par "htaccess_pour_free-fr" si vous êtes hébergé chez Free.fr, ou essayez de supprimer ce fichier.
+//set to '0' if your host does not manage url rewriting properly
+//and if the links between chapters do not work. You may use
+//"htaccess_example" to set up an .htaccess file at the root
+//of your site to enable URL rewriting.
+//
+//passer à '0' si votre hébergeur ne gère pas convenablement
+//la réécriture d'url et que les liens entre les chapitres
+//ne fonctionnent pas. Vous pouvez créer un fichier .htaccess
+//sur base de "htaccess_example" pour activer la réécriture d'URL.
 $UrlRewriting = '1';
 
 //number of pictures to preload starting from current picture
@@ -39,13 +46,15 @@ $ShowTitle = '1';
 
 //app id for integration of facebook-driven comments
 //must be specified to show comments
+//
 //identifiant facebook pour intégrer des commentaires via facebook
 //doit être spécifié pour montrer les commentaires
 $FacebookCommentsAppId = '';
 //'0' to delete the comments screen at the end of the episode
 //'1' to display comments of every episode at the end of the episode
 //'2' to display comments of every episode under the episode
-////'3' to display comments of every episode ---
+//'3' to display comments of every episode ---
+//
 //'0' pour supprimer l'écran de commentaires en fin d'épisode
 //'1' pour afficher les commentaires propres à chaque épisode en fin d'épisode
 //'2' pour afficher les commentaires propres à chaque épisode sous l'épisode
